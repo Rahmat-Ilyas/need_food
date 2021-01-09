4be7777a07cf36067f43d1569a61339470e93236
Rahmat-Ilyas <rahmat.ilyas142@gmail.com>
2021-01-10 00:35:22 +0800

    Tes Schedule

--- resources/app/Console/Kernel.php
+++ resources/app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Model\Pemesanan;
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +26,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            $pesanan = Pemesanan::first();
+            $pesanan->status = 'Newss';
+            $pesanan->save();
+        })->everyMinute();
     }
 
     /**
